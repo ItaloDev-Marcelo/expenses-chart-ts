e07351a862ae9ecdec37c8b389e60dc5f46ec6d7
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-13 14:39:05 -0300

    add style to BalanceHeader

--- src/balance/BalanceHeader.tsx
+++ src/balance/BalanceHeader.tsx
@@ -2,12 +2,12 @@ import logo from '../assets/images/logo.svg'
 
 const BalanceHeader = () => {
   return (
-    <header>
-        <div>
-            <h1>My balance</h1>
-            <p>$921.48</p>
+    <header className='bg-Primary-red flex justify-between items-center h-[90px] rounded-2xl w-[350px] md:w-[375px] xl:w-[400px] p-2'>
+        <div className='ml-3 text-Neutral-red-100'>
+            <h1 className='text-[.8em]'>My balance</h1>
+            <p  className='text-[1.125em] xl:text-[1.2em] mt-1 font-bold'>$921.48</p>
         </div>
-        <a href='#'><img src={logo} alt='logo' /></a>
+        <a href='#'><img src={logo} alt='logo' className='w-[75%]' /></a>
     </header>
   )
 }
