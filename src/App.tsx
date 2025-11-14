ec611a5fbcefa321e2636f19c5af8969f480aaae
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-14 15:11:54 -0300

    feat: add ExpensesChartLayout to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import BalanceHeader from './balance/BalanceHeader'
+import ExpensesChartLayout from './layout/ExpensesChartLayout'
 function App() {
  
   return (
      <div className='flex flex-col items-center justify-center mt-18 tb:mt-[5;5rem] md:mt-20 xl:mt-28 px-2'>
        <BalanceHeader/>
+       <ExpensesChartLayout/>
      </div>
   )
 }
