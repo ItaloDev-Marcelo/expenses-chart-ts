53405fea180b61ec48b82e053faf2453beb864d6
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-18 11:19:36 -0300

    refactor add selectChart and handleSelectorChart to a ChartElement

--- src/components/ChartElement.tsx
+++ src/components/ChartElement.tsx
@@ -1,11 +1,14 @@
 import type { Chart} from "../types/Chart.type"
 
-const ChartElement  = ({day, amount, style}:Chart) => {
+const ChartElement  = ({day, amount, style,selectChart,handleSelectChart}:Chart) => {
+
+  
+
   return (
-    <div className="flex flex-col items-center  ">
-      <div className="mb-1.5"><p>{amount}</p></div>
-      <div className={style}></div>
-      <p>{day}</p>
+    <div className="flex flex-col items-center">
+      <div className={selectChart === amount ? 'mb-1.5 bg-Neutral-Brown-950 text-Neutral-red-100 text-[.8em] p-1 font-bold  rounded-[5px]' : 'hidden'}><p>${amount}</p></div>
+      <div className={style} onClick={() => handleSelectChart(amount)}></div>
+      <p className="text-Neutral-Brown-400">{day}</p>
     </div>
   )
 }
