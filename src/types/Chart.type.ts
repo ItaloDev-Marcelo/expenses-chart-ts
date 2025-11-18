f19aa8c260d14d7fbf4acbcdb3adfdb216368fae
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-18 11:23:04 -0300

    refactor update and add ChartProps2 to Chart.type

--- src/types/Chart.type.ts
+++ src/types/Chart.type.ts
@@ -9,5 +9,10 @@ export type ChartProps = {
     style?: string
 } 
 
-export type  Chart = communProps & ChartProps;
+export type ChartProps2 = {
+    handleSelectChart: (text:number) => void, 
+    selectChart: number
+}
 
+export type  Chart = communProps & ChartProps &  ChartProps2;
+export type Chart2 = communProps & ChartProps2;
