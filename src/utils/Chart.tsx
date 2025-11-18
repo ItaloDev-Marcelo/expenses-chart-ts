9709e64253cc71193be80a831db8b1307f3248ad
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-18 11:21:23 -0300

    refactor add selectChart and handleSelectorChart to a Chart

--- src/utils/Chart.tsx
+++ src/utils/Chart.tsx
@@ -1,28 +1,29 @@
 import ChartElement from '../components/ChartElement'
-import type {communProps} from '../types/Chart.type'
+import type {Chart2} from '../types/Chart.type'
 
-const ChartSelect = ({day,amount}:communProps) => {
+
+const ChartSelect = ({day,amount,selectChart,handleSelectChart}:Chart2) => {
     switch(day) {
         case 'mon':
-         return <ChartElement day={day} amount={amount} style='  w-6.5 md:w-8  xl:w-9 h-7 bg-Primary-red mx-2  rounded-t-xs' />
+         return <ChartElement day={day} amount={amount} selectChart={selectChart} handleSelectChart={handleSelectChart} style='  w-6.5 md:w-8  xl:w-9 h-7 bg-Primary-red mx-2  rounded-t cursor-pointer hover:opacity-[0.7]' />
         break;
         case 'tue' :
-        return <ChartElement day={day} amount={amount} style='  w-6.5 md:w-8  xl:w-9 h-14 bg-Primary-red mx-2  rounded-t-xs' />
+        return <ChartElement day={day} amount={amount} selectChart={selectChart} handleSelectChart={handleSelectChart} style='  w-6.5 md:w-8  xl:w-9 h-14 bg-Primary-red mx-2  rounded-t cursor-pointer hover:opacity-[0.7]' />
         break;
         case 'wed' :
-        return <ChartElement day={day} amount={amount} style='  w-6.5 md:w-8  xl:w-9 h-26 bg-Primary-red mx-2  rounded-t-xs' />
+        return <ChartElement day={day} amount={amount} selectChart={selectChart} handleSelectChart={handleSelectChart} style='  w-6.5 md:w-8  xl:w-9 h-26 bg-Primary-blue mx-2  rounded-t cursor-pointer hover:opacity-[0.7]' />
         break;
         case 'thu' :
-        return <ChartElement day={day} amount={amount} style='  w-6.5 md:w-8  xl:w-9 h-12 bg-Primary-red mx-2  rounded-t-xs' />
+        return <ChartElement day={day} amount={amount} selectChart={selectChart} handleSelectChart={handleSelectChart} style='  w-6.5 md:w-8  xl:w-9 h-12 bg-Primary-red mx-2  rounded-t cursor-pointer hover:opacity-[0.7]' />
         break;
         case 'fri' :
-        return <ChartElement day={day} amount={amount} style='  w-6.5 md:w-8  xl:w-9 h-9 bg-Primary-red mx-2  rounded-t-xs' />
+        return <ChartElement day={day} amount={amount} selectChart={selectChart} handleSelectChart={handleSelectChart} style='  w-6.5 md:w-8  xl:w-9 h-9 bg-Primary-red mx-2  rounded-t cursor-pointer hover:opacity-[0.7]' />
         break;
         case 'sat' :
-        return <ChartElement day={day} amount={amount} style='  w-6.5 md:w-8  xl:w-9 h-20 bg-Primary-red mx-2  rounded-t-xs' />
+        return <ChartElement day={day} amount={amount} selectChart={selectChart} handleSelectChart={handleSelectChart} style='  w-6.5 md:w-8  xl:w-9 h-20 bg-Primary-red mx-2  rounded-t cursor-pointer hover:opacity-[0.7]' />
         break;
         case 'sun' :
-        return <ChartElement day={day} amount={amount} style='  w-6.5 md:w-8  xl:w-9 h-13 bg-Primary-red mx-2  rounded-t-xs' />
+        return <ChartElement day={day} amount={amount} selectChart={selectChart} handleSelectChart={handleSelectChart} style='  w-6.5 md:w-8  xl:w-9 h-13 bg-Primary-red mx-2  rounded-t cursor-pointer hover:opacity-[0.7]' />
         break;
     }
 }
