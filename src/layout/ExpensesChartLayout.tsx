859393339a0747c41d0bb06077344f88d816a2ab
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-19 14:39:56 -0300

    style:fix style and add scale to let bigger

--- src/layout/ExpensesChartLayout.tsx
+++ src/layout/ExpensesChartLayout.tsx
@@ -3,14 +3,14 @@ import Data from '../data.json'
 import Chart from '../utils/Chart'
 const ExpensesChartLayout = () => {
 
-    const [selectChart,setSelectChart] = useState(52.36);
+    const [selectChart,setSelectChart] = useState(0);
     const handleSelectChart = (index:number) => setSelectChart(index)
 
     return (
-        <main className='w-[325px] md:w-[375px] xl:w-[400px] bg-white rounded-2xl'>
-            <section className="w-full p-5 space-y-2.5  ">
+        <main className='w-[325px] md:w-[375px] xl:w-[400px] bg-white rounded-2xl ds:scale-125'>
+            <section className="w-full p-5 space-y-2.5   ">
                 <div className='mb-3 my-1'>
-                    <h2 className='mb-5 text-[1.3rem] md:text-[1.5rem] xl:text-[1.6rem] font-bold mb-1.5  '>Speding - Last 7 days</h2>
+                    <h2 className='mb-5 text-[1.3rem] md:text-[1.5rem] xl:text-[1.6rem] font-bold '>Speding - Last 7 days</h2>
                    <div className='flex flex-row justify-center items-end mt-1 h-35 '>
                      {Data.map((valor, index) => (
                          <Chart day={valor.day} amount={valor.amount} key={index} 
