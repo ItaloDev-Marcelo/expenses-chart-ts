1f73de87d850e071bd324c670b6dc288b76c1236
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-19 14:37:52 -0300

    style:add a style fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ExpensesChartLayout from './layout/ExpensesChartLayout'
 function App() {
  
   return (
-     <div className='flex flex-col items-center justify-center mt-18 tb:mt-[5;5rem] md:mt-20 xl:mt-28 px-2'>
+     <div className='flex flex-col items-center justify-center mt-18 tb:mt-22 md:mt-20  px-2 '>
        <BalanceHeader/>
        <ExpensesChartLayout/>
      </div>
