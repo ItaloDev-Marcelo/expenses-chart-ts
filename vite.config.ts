4e86be3384e52315fefe2d4e87b37ed73d256b7d
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-02 15:06:26 -0300

    chore: add testing library and configs to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vitest/config'
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+  },
 })
