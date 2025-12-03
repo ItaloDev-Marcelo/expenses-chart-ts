887ffa8b9b9f337cfb67eab1ddbe2380cf8f2ff5
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-03 11:03:24 -0300

    refactor: change name Chart to ChartSelect on ExpensesChartLayout

--- src/layout/ExpensesChartLayout.tsx
+++ src/layout/ExpensesChartLayout.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Data from '../data.json'
-import Chart from '../utils/Chart'
+import ChartSelect  from '../utils/Chart'
 const ExpensesChartLayout = () => {
 
     const [selectChart,setSelectChart] = useState(0);
@@ -13,14 +13,14 @@ const ExpensesChartLayout = () => {
                     <h2 className='mb-5 text-[1.3rem] md:text-[1.5rem] xl:text-[1.6rem] font-bold '>Speding - Last 7 days</h2>
                    <div className='flex flex-row justify-center items-end mt-1 h-35 '>
                      {Data.map((valor, index) => (
-                         <Chart day={valor.day} amount={valor.amount} key={index} 
+                         <ChartSelect  day={valor.day} amount={valor.amount} key={index} 
                          selectChart={selectChart} handleSelectChart={handleSelectChart}
                           />
                     ))}
                    </div>
                 </div>
                 <hr />
-                <div className='flex flex-row justify-between p-2'>
+                <div className='flex flex-row justify-between p-2' id='total'>
                     <div>
                         <p className='text-Neutral-Brown-400'>Total this month</p>
                         <h3 className='text-Neutral-Brown-950 text-[1.7em] font-bold'>$478.33</h3>
