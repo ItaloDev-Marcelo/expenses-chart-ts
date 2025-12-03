fc21b2d3995897a4a86c2a7e6fc9639601962f5f
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-03 11:02:18 -0300

    refactor: add arial-label and data-testid

--- src/components/ChartElement.tsx
+++ src/components/ChartElement.tsx
@@ -1,13 +1,12 @@
 import type { Chart} from "../types/Chart.type"
 
 const ChartElement  = ({day, amount, style,selectChart,handleSelectChart}:Chart) => {
-
-  
-
   return (
     <div className="flex flex-col items-center">
-      <div className={selectChart === amount ? 'mb-1.5 bg-Neutral-Brown-950 text-Neutral-red-100 text-[.8em] p-1 font-bold  rounded-[5px]' : 'hidden'}><p>${amount}</p></div>
-      <div className={style} onClick={() => handleSelectChart(amount)}></div>
+      <div className={selectChart === amount ? 
+        'mb-1.5 bg-Neutral-Brown-950 text-Neutral-red-100 text-[.8em] p-1 font-bold  rounded-[5px]'
+         : 'hidden'}><p>${amount}</p></div>
+      <div className={style} aria-label="Chart-button" data-testid="chart-bar" onClick={() => handleSelectChart(amount)}></div>
       <p className="text-Neutral-Brown-400">{day}</p>
     </div>
   )
